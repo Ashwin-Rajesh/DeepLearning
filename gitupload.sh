da79cc13fb5263ce63fc77a7525ab177ca6b4ef6
Ashwin Rajesh <aswinrajesh1@gmail.com>
2020-03-08 21:49:52 +0530

    Automated commit from bash script

--- gitupload.sh
+++ gitupload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /home/ashwin/Projects/DeepLearning
 git add `ls`
 git commit -m "Automated commit from bash script"
 git remote add origin https://github.com/Ashwin-Rajesh/DeepLearning.git
